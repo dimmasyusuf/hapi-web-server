e470c0c9f568a626ce4a2afe1a42987d04a48ba9
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-16 10:47:45 +0700

    feat: response toolkit

--- routes.js
+++ routes.js
@@ -50,6 +50,13 @@ const routes = [
     },
   },
   {
+    method: 'POST',
+    path: '/user',
+    handler: (request, h) => {
+      return h.response('created').code(201);
+    },
+  },
+  {
     method: '*',
     path: '/{any*}',
     handler: (request, h) => {
